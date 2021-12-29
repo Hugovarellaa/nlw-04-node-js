550b6bb4efc4369b7c9a4834daa622b3fa9c6987
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-12-29 16:14:46 -0300

    Segunda Aula
    Completa

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
+import "reflect-metadata";
+import "./database";
 import express from "express";
+import { router } from "./routes";
 
 const app = express();
-
-app.get("/users", (reqquest, response) => {
-  return response.send({ message: "Certinho caralho!!!!" });
-});
-
-app.post("/users", (reqquest, response) => {
-  return response.json({ message: "Certinho caralho" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running"));
